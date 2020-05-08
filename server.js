9b3426bfee91a10ba71ec780caf8082b141740d9
bencetuzson <tuzsonbence2003@gmail.com>
2020-05-08 22:50:02 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.use("/", express.static("public"));
+
+app.listen(port, () => {
+    console.log(`Server listening on PORT ${port}`)
+});
